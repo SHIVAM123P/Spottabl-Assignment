e52efde45c631461db877323a86d3eaad4945f91
SHIVAM123P <kshivam12112000@gmail.com>
2022-05-14 21:46:50 +0530

    Added some styling and functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
+//import './add.js';
 import JSONDATA from './MOCK_DATA.json';
 import {useState} from 'react' 
 function App() {
@@ -10,31 +11,98 @@ function App() {
       <div className='heading'>
 
       
-      <h2>Your SPOTTABLE Team</h2>
-      <h6>SPOTTABLE supports you all througout</h6>
+      <h2>Your SPOTTABL Team</h2>
+      <h6>SPOTTABL supports you all througout</h6>
       </div>
       <div className='box'>
       <h3 className='heading3'>Customer Success Managers</h3>
 
-      <input type="text" placeholder="Add by Name or email" onChange={event=>{setSearchTerm(event.target.value)}} />
+      <input id='search' type="text" placeholder="Add by Name or email" onChange={event=>{setSearchTerm(event.target.value)}} />
+      <hr></hr>
+      <a ><button  className='button' >Add CSM</button> </a>
 
       {JSONDATA.filter((val)=>{
-        if(searchTerm==""){
-          return val
+        if(searchTerm===""){
+          return val;
         }
         else if(val.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
-          return val
+          return val;
         }
       }).map((val,key)=>{
         return(
 
-         <div className='user' key={key}><p>{val.first_name }{" "}{val.last_name} </p>
+         <div className='user' key={key}>
+           <p>{val.first_name }{" "}{val.last_name}<br/> {val.Position} <hr></hr></p>
         </div>
-        );
-      })};
+        )
+      })}
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+// import React, { Component } from "react";
+// import { Form, Button } from 'semantic-ui-react';
+// import jsonfile from'jsonfile';
+
+// var file = 'MOCK_DATA.json'
+
+// var obj = {name: 'JP'}
+
+
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props)
+
+//     this.state = {
+//       name: "",
+//       email: ""
+//     }
+
+//     this.handleChange = this.handleChange.bind(this);
+//     this.sendDataSomewhere = this.sendDataSomewhere.bind(this);
+// }
+
+
+// handleChange = (e, {name, value}) => {
+//   this.setState({[name]: value})
+// }
+
+// sendDataSomewhere = function jsonfile(file){
+// jsonfile.writeFile(file, obj, function (err) {
+//   console.error(err);
+// });
+// };
+
+
+
+//   render() {
+//     return (
+//      <div>
+//       <Form onSubmit={this.sendDataSomewhere}>
+//         <Form.Field>
+//           <Form.Input name="name" value={this.state.name} onChange={this.handleChange}/>
+//         </Form.Field>
+//         {/* <Form.Field>
+//           <Form.Input name="email" value={this.state.email} onChange={this.handleChange}/>
+//         </Form.Field> */}
+//         <Button type="submit">Submit</Button>
+//        </Form>
+//      </div>
+//     );
+//   }
+// }
+
+// const handleSaveToPC = (jsonData,filename) => {
+//   const fileData = JSON.stringify(jsonData);
+//   const blob = new Blob([fileData], {type: "text/plain"});
+//   const url = URL.createObjectURL(blob);
+//   const link = document.createElement('a');
+//   link.download = `${filename}.json`;
+//   link.href = url;
+//   link.click();
+// }
---
+++ src/add.js
@@ -0,0 +1,25 @@
+// import './App,js'
+
+
+
+// const fs = require("fs");
+  
+// // Storing the JSON format data in myObject
+// var data = fs.readFileSync("MOCK_DATA.json");
+// var myObject = JSON.parse(data);
+  
+// // Defining new data to be added
+// let newData = {
+//     "first_name":document.getElementById('input').value
+// };
+  
+// // Adding the new data to our object
+// myObject.push(newData);
+  
+// // Writing to our JSON file
+// var newData2 = JSON.stringify(myObject);
+// fs.writeFile("data2.json", newData2, (err) => {
+//   // Error checking
+//   if (err) throw err;
+//   console.log("New data added");
+// });
